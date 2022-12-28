6ff5cfb5abaa3a4572b83bd7f777c2bbdf905a6f
shirakaba <14055146+shirakaba@users.noreply.github.com>
2022-12-28 12:04:59 +0900

    Proxy classes from global object and support console.log

--- demo.js
+++ demo.js
@@ -1,14 +1,83 @@
-const { objc, sel } = require('.');
+const { objc, sel } = require(".");
 
 const { getClass, msgSend } = objc;
 const { registerName } = sel;
 
-const NSString = getClass('NSString');
+const classes = new Proxy(
+  {},
+  {
+    get(target, className, receiver) {
+      console.log(`Get className ${className}`);
+      /** @type {Buffer} */
+      const classInstance = getClass(className);
 
-const stringA = msgSend(
-    msgSend(NSString, registerName('alloc')),
-    registerName('initWithString:'),
-    'Hello',
+      return new Proxy(
+        {
+          instance: classInstance,
+          toString() {
+            let address = [];
+            for (let i = 0, length = classInstance.length; i < length; i++) {
+              address.push(`${classInstance[i].toString(16).padStart(2, "0")}`);
+            }
+
+            return `<${className} ${address.join(" ")}>`;
+          },
+          [Symbol.for("nodejs.util.inspect.custom")](
+            depth,
+            inspectOptions,
+            inspect
+          ) {
+            return this.toString();
+          },
+        },
+        {
+          // `new classes.NSString()` -> `classes.NSString.alloc().init()`
+          construct(target, args) {
+            console.log(`construct()`, ...args);
+            // return msgSend(
+            //   msgSend(target, registerName('alloc')),
+            //   registerName('init'),
+            // );
+            return target.instance.alloc().init();
+          },
+          has(target, prop, receiver) {
+            console.log(`has('${prop}')`);
+            return Reflect.has(...arguments);
+          },
+          getOwnPropertyDescriptor(target, prop) {
+            console.log(`getOwnPropertyDescriptor('${prop}')`);
+            return Reflect.getOwnPropertyDescriptor(...arguments);
+          },
+          getPrototypeOf(target, prop, receiver) {
+            console.log(`getPrototypeOf('${prop}')`);
+            return Reflect.getPrototypeOf(...arguments);
+          },
+          ownKeys(target) {
+            console.log(`ownKeys('${prop}')`);
+            return Reflect.ownKeys(...arguments);
+          },
+          get(target, prop, receiver) {
+            console.log(`get('${prop}')`);
+            if (prop === "toString") {
+              return Reflect.get(...arguments);
+            }
+            return (...args) =>
+              msgSend(target.instance, registerName(prop), ...args);
+          },
+        }
+      );
+    },
+  }
 );
 
-console.log(stringA);
+const str = classes.NSString;
+const alloc = str.alloc();
+// const init = str.alloc().init();
+// const nue = new str();
+
+console.log(str);
+console.log(alloc);
+// console.log(init);
+console.log(nue);
+
+// console.log(classes.NSString);
